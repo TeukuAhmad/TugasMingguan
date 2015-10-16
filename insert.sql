b3062311fa651f400d13c17508fb76ea2733b17a
TeukuAhmad <teuku.ahmad@rocketmail.com>
2015-10-16 14:28:14 +0700

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,2 +1,3 @@
 insert into tugas_mingguan values ("10104007","Adam MB","2015-10-28 09:31"),
+("10112486","TeukuMuhammadShalahuddin","2015-10-16 14:27"),
 /* baris 1 hanya contoh perintah yang dimasukan, silakan tambahkan data anda setelah baris ini */
